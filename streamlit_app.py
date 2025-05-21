3d898fd91a9846703f8ff08675ebc204a2f46211
Juan Domandl <juan.domandl@mi.unc.edu.ar>
2025-05-21 18:21:44 +0000

    dwld

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,28 @@
 import streamlit as st
 from lafama import download_productos, create_df_products, add_missing_columns
+import pandas as pd
+from io import BytesIO
 
 st.title("🧉 La Fama (Minorista)")
 
-if st.button("Crear excel"):
+if st.button("Descargar datos mayorista"):
     all_products = download_productos()
     df = create_df_products(all_products)
     df = add_missing_columns(df)
 
-    st.dataframe(df)  # This will display the DataFrame as an interactive table
+    st.dataframe(df)  # Mostrar tabla interactiva
+
+    # Convertir a Excel en memoria
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Productos')
+        writer.save()
+    output.seek(0)
+
+    # Botón para descargar archivo
+    st.download_button(
+        label="📥 Descargar como Excel",
+        data=output,
+        file_name="planilla.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
